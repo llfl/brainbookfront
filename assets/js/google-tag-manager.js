368e582109c3f8ebee66200c00486eac893026ad
Kun <quinnHugh@hotmail.com>
2019-10-16 14:44:28 +0800

    init update

---
+++ assets/js/google-tag-manager.js
@@ -0,0 +1,1 @@
+var trackingId="UA-30038099-8";addLoadEvent(loadTracking);function addLoadEvent(a){var b=window.onload;if(typeof window.onload!=="function"){window.onload=a}else{window.onload=function(){b();a()}}}function loadTracking(){window.dataLayer=window.dataLayer||[];function a(){dataLayer.push(arguments)}a("js",new Date());a("config",trackingId)};
